179331609ae2c713f427838c6ef5b4f859ba3bc8
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-08-06 03:46:40 +0100

    Update Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,5 +1,5 @@
 ./sonicheroes.utils.discordrpc/Publish.ps1 @args `
 -UseScriptDirectory false `
 -ProjectPath "sonicheroes.utils.discordrpc/sonicheroes.utils.discordrpc.csproj" `
--PackageName "sonicheroes.utils.discordrpc" `
+-PackageName "sonicheroes.discordrichpresence" `
 -ReadmePath README-MOD.md
