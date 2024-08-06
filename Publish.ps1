33415dad9937f8ed31ac7e1aef3d92239c9b94da
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-08-06 04:57:05 +0100

    Fixed ./Publish.ps1

--- Publish.ps1
+++ Publish.ps1
@@ -1,4 +1,4 @@
-./sonicheroes.utils.discordrpc/Publish.ps1 @args `
+./sonicheroes.discordrichpresence/Publish.ps1 @args `
 -UseScriptDirectory false `
 -ProjectPath "sonicheroes.discordrichpresence/sonicheroes.discordrichpresence.csproj" `
 -PackageName "sonicheroes.discordrichpresence" `
