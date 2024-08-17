be8a6b744e33124b641676d6a2bfe90af50515af
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-08-17 18:32:38 +0100

    .

--- sonicheroes.discordrichpresence/Publish.ps1
+++ sonicheroes.discordrichpresence/Publish.ps1
@@ -173,8 +173,8 @@ param (
     $UseGameBananaDelta = $True,
     $UseNuGetDelta = $False,
 
-    $GitHubUserName = "Auxy6858", # Name of the GitHub user where the mod is contained
-    $GitHubRepoName = "SonicHeroes.discordrichpresence.ReloadedII", # Name of the GitHub repo where the mod is contained
+    $GitHubUserName = "", # Name of the GitHub user where the mod is contained
+    $GitHubRepoName = "", # Name of the GitHub repo where the mod is contained
     $GitHubFallbackPattern = "", # For migrating from legacy build script.
     $GitHubInheritVersionFromTag = $True, # Uses version determined from release tag as opposed to metadata file in latest release.
 
